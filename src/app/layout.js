8bb533fea1170fc0588bcb23e67d068591221a92
Ajey <ajejey@gmail.com>
2024-08-10 23:02:41 +0530

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { PieChart } from "lucide-react";
+import GoogleAnalytics from "@/GoogleAnalytics/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,39 +14,40 @@ export const metadata = {
   openGraph: {
     title: 'Financial Planning Made Easy | Your Path to Financial Freedom',
     description: 'Plan your financial future with our intuitive tool. Visualize savings, investments, and retirement plans.',
-    images: [{ url: '/path-to-og-image.jpg' }],
+    images: [{ url: '../../public/vercel.svg' }],
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Financial Planning Made Easy',
     description: 'Plan your financial future with our intuitive tool.',
-    images: ['/path-to-twitter-image.jpg'],
+    images: ['../../public/vercel.svg'],
   },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics />
       <body className="bg-gray-100 text-gray-900 font-sans bg-gradient-to-b from-blue-50 to-white min-h-screen">
-      <header className="pt-2">
-        <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-          <div className="flex items-center">
-            <PieChart className="h-8 w-8 text-blue-600 mr-2" />
-            <span className="font-bold text-xl text-gray-800">FinPlanner</span>
-          </div>
-          {/* <div className="flex space-x-4">
+        <header className="pt-2">
+          <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+            <div className="flex items-center">
+              <PieChart className="h-8 w-8 text-blue-600 mr-2" />
+              <span className="font-bold text-xl text-gray-800">FinPlanner</span>
+            </div>
+            {/* <div className="flex space-x-4">
             <Link href="/about" className="text-gray-600 hover:text-gray-900">About</Link>
             <Link href="/blog" className="text-gray-600 hover:text-gray-900">Blog</Link>
             <Link href="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link>
           </div> */}
-        </nav>
-      </header>
+          </nav>
+        </header>
         {/* <div className="flex justify-center gap-4 p-4">
           <Link className="text-blue-500 hover:text-blue-700 bg-blue-100 hover:bg-blue-200 rounded px-3 py-1 hover:shadow-md" href="/" activeClassName="bg-blue-200 shadow-md">Home</Link>
           <Link className="text-blue-500 hover:text-blue-700 bg-blue-100 hover:bg-blue-200 rounded px-3 py-1 hover:shadow-md" href='/financial-planner' activeClassName="bg-blue-200 shadow-md">Financial Planner</Link>
         </div> */}
         {children}
-        </body>
+      </body>
     </html>
   );
 }
